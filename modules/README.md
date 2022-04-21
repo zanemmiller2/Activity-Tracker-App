e25c15b24f423dfde1b3976727cf091e7bfa64ff
Zane Miller <85970128+zanemmiller2@users.noreply.github.com>
2022-04-21 01:03:37 -0700

    Add files via upload

---
+++ modules/README.md
@@ -0,0 +1,9 @@
+1. Sources:
+
+    1. https://dev.mysql.com/doc/
+        *     MySQL Documentation
+    2. https://github.com/markwk/qs_ledger/blob/master/apple_health/apple-health-data-parser.py
+        *     Inspiration - this helped me understand the different types of data that could be 
+              extracted from Apple Health and the general XML structures of different types
+    3. https://www.geeksforgeeks.org/python-mariadb-insert-into-table-using-pymysql/
+        *     Python MariaDB – Insert into Table using PyMySQL 
---
+++ modules/user_interface_example.py
@@ -0,0 +1,46 @@
+from FilteredUploadClass import FilterDataUpload
+
+if __name__ == '__main__':
+    unfiltered_csv = '/Users/zanemiller/Desktop/Activity Tracker App/apple_health_export/' \
+                     'Extracted_data/Workout.csv'
+    filtered_csv = '/Users/zanemiller/Desktop/Activity Tracker App/apple_health_export/' \
+                   'Filtered_Data/Workout_Filtered.csv'
+
+    data_tables_types_sources = {
+        'workout': ['Zane’s Apple\xa0Watch', 'Stryd', 'Slopes', 'ELEMNT'],
+        'sleep': ['Autosleep', 'Zane’s Apple\xa0Watch'],
+        'resting_hr': ['Zane’s Apple\xa0Watch']}
+    upload_data_type = 'workout'
+
+    # --------------------------------------------------------------------------------------------#
+    #   This section models the API for uploading workout data from Apple Health                  #
+    # --------------------------------------------------------------------------------------------#
+
+    # upload data to a specific table TODO ('sleep', 'resting_hr' - functions)
+    workout_upload = FilterDataUpload(unfiltered_csv, filtered_csv, upload_data_type)
+
+    # filters out unwanted import sources from watch workout data ** optional param
+    workout_upload.filter_watch_workout_data(data_tables_types_sources[upload_data_type])
+
+    user = "zanemiller"
+    password = "230Leafwoodct!"
+    host = "10.0.0.208"
+    port = 3306
+    database = 'fitnessTracker'
+
+    workout_upload.database_connect(user=user,
+                                    password=password,
+                                    host=host,
+                                    port=port,
+                                    database=database)
+
+    # creates new table if one does not already exist
+    workout_upload.create_new_table('workout_table')
+
+    workout_upload.upload_csv()
+
+    workout_upload.commit_changes()
+
+    workout_upload.close_connection()
+
+    # TODO Write class for analyzing data
